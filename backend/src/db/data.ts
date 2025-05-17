15da53b9f5379ea74e7f07babefa4f2fff56d21a
aiminhTech <minhquach582@gmail.com>
2025-05-17 20:35:30 +0200

    backend: fix db

--- backend/src/db/data.ts
+++ backend/src/db/data.ts
@@ -1,32 +1,46 @@
-import type { Category, CheckList, CheckListItem, Image, Information, Language, Quiz, QuizAnswer, QuizQuestion } from "./model";
+import type {
+  Category,
+  CheckList,
+  CheckListItem,
+  Image,
+  Information,
+  Language,
+  Quiz,
+  QuizAnswer,
+  QuizQuestion,
+} from "@models/model";
 
-export const locales = ["en", "de"]
+export const locales = ["en", "de"];
 
 export const languages: Language[] = [
-	{ localeId: 1, name: "English" },
-	{ localeId: 1, name: "German" },
-	{ localeId: 2, name: "Englisch" },
-	{ localeId: 2, name: "Deutsch" },
+  { localeId: 1, name: "English" },
+  { localeId: 1, name: "German" },
+  { localeId: 2, name: "Englisch" },
+  { localeId: 2, name: "Deutsch" },
 ];
 
 export const categories: Category[] = [
-	{ localeId: 1, name: "Culture", description: "foo" },
-	{ localeId: 1, name: "Work", description: "foo" },
-	{ localeId: 2, name: "Kultur", description: "foo" },
-	{ localeId: 2, name: "Arbeiten", description: "foo" },
-	{ localeId: 2, name: "Behördengänge", description: "foo" },
-	{ localeId: 2, name: "Administrativen", description: "foo" },
+  { localeId: 1, name: "Culture", description: "foo" },
+  { localeId: 1, name: "Work", description: "foo" },
+  { localeId: 2, name: "Kultur", description: "foo" },
+  { localeId: 2, name: "Arbeiten", description: "foo" },
+  { localeId: 2, name: "Behördengänge", description: "foo" },
+  { localeId: 2, name: "Administrativen", description: "foo" },
 ];
 
 export const informations: Information[] = [
-	{
-		categoryId: 1, title: "Contract, reference, probation period", content: [
-			{
-				topic: "Employment contract and collective agreements",
-				description: "The employee and the employer must sign a contract specifying the terms of employment. In some sectors, specific rules predefined by national or regional collective agreements apply.",
-				infos: [{
-					subtitle: "Individual employment contract",
-					text: `
+  {
+    categoryId: 1,
+    title: "Contract, reference, probation period",
+    content: [
+      {
+        topic: "Employment contract and collective agreements",
+        description:
+          "The employee and the employer must sign a contract specifying the terms of employment. In some sectors, specific rules predefined by national or regional collective agreements apply.",
+        infos: [
+          {
+            subtitle: "Individual employment contract",
+            text: `
 					New employees and their employers who hire a new employee need to sign an employment contract. This contract sets out the rights and obligations that apply to both parties in the employment relationship.
 
 					The contract must include:
@@ -39,128 +53,134 @@ export const informations: Information[] = [
 					- For fixed-term contracts, the end date
 
 					> Additional special rules must be included, such as a non-compete clause or rules on overtime.
-					`
+					
 				},
 				{
 					subtitle: "Collective employment agreements",
-					text: `
+					text: 
 					A collective employment agreement is an agreement concluded between employers’ associations and workers’ associations / unions. The two parties define the minimum conditions (e.g. minimum wage) to be observed. In Switzerland, there are many collective employment agreements, including collective employment agreements for hairdressers, the construction industry, private security and the bakery-pastry sector.
 
 					These collective employment agreements are generally binding to a specific sector throughout Switzerland. There also company-specific collective employment agreements (e.g. Coop or Migros collective employment agreements) and collective employment agreements specific to a certain region, with conditions that only apply in a specific company or region.
-					`
-				}
-				]
-			},
-		]
-	},
+					`,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    categoryId: 4,
+    title: "Vertrag, Bezug, Probezeit",
+    content: [
+      {
+        topic: "Arbeitsvertrag und Gesamtarbeitsverträge",
+        description:
+          "Der Arbeitnehmer und der Arbeitgeber müssen einen Vertrag unterschreiben, der die Arbeitsbedingungen festlegt. In einigen Branchen gelten spezielle Regeln, die in nationalen oder regionalen Gesamtarbeitsverträgen vordefiniert sind.",
+        infos: [
+          {
+            subtitle: "Individueller Arbeitsvertrag",
+            text: `
+Neue Mitarbeitende und ihre Arbeitgeber müssen einen Arbeitsvertrag unterschreiben. Dieser Vertrag legt die Rechte und Pflichten beider Parteien im Arbeitsverhältnis fest.
+
+Der Vertrag muss Folgendes enthalten:
+
+- Name des Arbeitnehmers und des Arbeitgebers  
+- Beginn des Arbeitsverhältnisses  
+- Funktion des Arbeitnehmers  
+- Lohn und allfällige Zulagen  
+- Wöchentliche Arbeitszeit  
+- Bei befristeten Verträgen das Enddatum
+
+> Zusätzliche Sonderregelungen müssen gegebenenfalls enthalten sein, wie z.B. eine Wettbewerbsverbotsklausel oder Regeln zu Überstunden.
+            `,
+          },
+          {
+            subtitle: "Gesamtarbeitsverträge",
+            text: `
+Ein Gesamtarbeitsvertrag ist eine Vereinbarung zwischen Arbeitgeber- und Arbeitnehmerverbänden / Gewerkschaften. Die beiden Parteien definieren die Mindestbedingungen (z.B. Mindestlohn), die einzuhalten sind. In der Schweiz gibt es viele Gesamtarbeitsverträge, u.a. für Coiffeure, die Bauindustrie, privaten Sicherheitsdienst und das Bäckerei-Confiseur-Gewerbe.
+
+Diese Gesamtarbeitsverträge sind in der Regel für eine bestimmte Branche in der ganzen Schweiz verbindlich. Es gibt auch branchenspezifische Gesamtarbeitsverträge (z.B. Coop oder Migros GAV) und regional spezifische Gesamtarbeitsverträge, die nur für bestimmte Unternehmen oder Regionen gelten.
+            `,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    categoryId: 4,
+    title: "Arbeitszeiten und Pausen",
+    content: [
+      {
+        topic: "Arbeitszeitgesetzgebung",
+        description:
+          "Das Arbeitszeitgesetz regelt die maximale Arbeitszeit und die Mindestpausen, die ein Arbeitnehmer einhalten muss.",
+        infos: [
+          {
+            subtitle: "Maximale Arbeitszeit",
+            text: `
+In der Schweiz darf die tägliche Arbeitszeit in der Regel 9 Stunden nicht überschreiten. In bestimmten Branchen können abweichende Regeln gelten.
+
+Überstunden sind grundsätzlich zu vermeiden und müssen meist gesondert kompensiert werden.
+            `,
+          },
+          {
+            subtitle: "Pausenregelungen",
+            text: `
+Arbeitnehmende haben Anspruch auf Pausen bei einer Arbeitszeit von mehr als 5.5 Stunden. Die Pausen sind in der Regel mindestens 15 Minuten bei einer Arbeitszeit von 5.5 bis 7 Stunden und mindestens 30 Minuten ab 7 Stunden.
+
+> Pausen gelten nicht als Arbeitszeit und sind entsprechend freizugeben.
+            `,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export const checklists: CheckList[] = [
-	{
-		categoryId: 1,
-		title: "checklist 1",
-	},
-	{
-		categoryId: 3,
-		title: "Checkliste 1",
-	}
-]
+  { categoryId: 1, title: "checklist 1" },
+  { categoryId: 3, title: "Checkliste 1" },
+];
 
 export const checklistItems: CheckListItem[] = [
-	{
-		checklistId: 1,
-		text: "todo 1",
-	},
-	{
-		checklistId: 1,
-		text: "todo 2",
-	},
-	{
-		checklistId: 1,
-		text: "todo 2",
-	},
-	{
-		checklistId: 2,
-		text: "Schritt 1",
-	}, {
-		checklistId: 2,
-		text: "Schritt 2",
-	}, {
-		checklistId: 2,
-		text: "Schritt 3",
-	},
-]
+  { checklistId: 1, text: "todo 1" },
+  { checklistId: 1, text: "todo 2" },
+  { checklistId: 1, text: "todo 2" },
+  { checklistId: 2, text: "Schritt 1" },
+  { checklistId: 2, text: "Schritt 2" },
+  { checklistId: 2, text: "Schritt 3" },
+];
 
 export const quizzes: Quiz[] = [
-	{
-		categoryId: 1,
-		title: "Quiz 1"
-	},
-	{
-		categoryId: 1,
-		title: "Quiz 2"
-	},
-	{
-		categoryId: 3,
-		title: "Wissenstest 1" 
-	},
-	{
-		categoryId: 3,
-		title: "Wissenstest 2"
-	}
-]
+  { categoryId: 1, title: "Quiz 1" },
+  { categoryId: 1, title: "Quiz 2" },
+  { categoryId: 3, title: "Wissenstest 1" },
+  { categoryId: 3, title: "Wissenstest 2" },
+];
 
 export const quizQuestions: QuizQuestion[] = [
-	{
-		quizId: 1,
-		question: "question 1"
-	},
-	{
-		quizId: 1,
-		question: "question 2"
-	},
-	{
-		quizId: 2,
-		question: "Frage 1"
-	},
-	{
-		quizId: 2,
-		question: "Frage 2"
-	},
-]
+  { quizId: 1, question: "question 1" },
+  { quizId: 1, question: "question 2" },
+  { quizId: 2, question: "Frage 1" },
+  { quizId: 2, question: "Frage 2" },
+];
 
 export const quizAnswers: QuizAnswer[] = [
-	{
-		quizQuestionId: 1,
-		answer: "answer A",
-		isCorrect: 0
-	},
-	{
-		quizQuestionId: 1,
-		answer: "answer B",
-		isCorrect: 0
-	},
-	{
-		quizQuestionId: 1,
-		answer: "answer C",
-		isCorrect: 0
-	},
-	{
-		quizQuestionId: 1,
-		answer: "answer D",
-		isCorrect: 1
-	}
-]
+  { quizQuestionId: 1, answer: "answer A", isCorrect: 0 },
+  { quizQuestionId: 1, answer: "answer B", isCorrect: 0 },
+  { quizQuestionId: 1, answer: "answer C", isCorrect: 0 },
+  { quizQuestionId: 1, answer: "answer D", isCorrect: 1 },
+	{ quizQuestionId: 2, answer: "answer AA", isCorrect: 0 },
+  { quizQuestionId: 2, answer: "answer BB", isCorrect: 0 },
+  { quizQuestionId: 2, answer: "answer CC", isCorrect: 0 },
+  { quizQuestionId: 2, answer: "answer DD", isCorrect: 1 },
+	{ quizQuestionId: 3, answer: "answer AA", isCorrect: 0 },
+  { quizQuestionId: 3, answer: "answer BB", isCorrect: 0 },
+  { quizQuestionId: 3, answer: "answer CC", isCorrect: 0 },
+  { quizQuestionId: 3, answer: "answer DD", isCorrect: 1 },
+];
 
-export const images : Image[] = [
-	{
-		relatedTable: "information",
-		relatedId: 1,
-		url: "/src/assets/foo.png"
-	},
-	{
-		relatedTable: "information",
-		relatedId: 2,
-		url: "/src/assets/bar.png"
-	}
-]
+export const images: Image[] = [
+  { relatedTable: "information", relatedId: 1, url: "/src/assets/foo.png" },
+  { relatedTable: "information", relatedId: 2, url: "/src/assets/bar.png" },
+];
--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,4 +1,9 @@
 import { Database } from "bun:sqlite";
+import { join } from "path";
+import { insertAll } from "@db/insert";
+
+const dbPath = join(import.meta.dir, "../../swissway.db");
+const db = new Database(dbPath);
 
 const schema = `
 	PRAGMA foreign_keys = ON;
@@ -7,7 +12,6 @@ const schema = `
 		id INTEGER PRIMARY KEY NOT NULL,
 		code TEXT NOT NULL
 	) STRICT;
-
 	CREATE UNIQUE INDEX IF NOT EXISTS idx_locale_code ON locale(code);
 
 	CREATE TABLE IF NOT EXISTS language (
@@ -15,6 +19,7 @@ const schema = `
 		locale_id INTEGER NOT NULL REFERENCES locale(id),
 		name TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_language_locale_id_name ON language(locale_id, name);
 
 	CREATE TABLE IF NOT EXISTS category (
 		id INTEGER PRIMARY KEY NOT NULL,
@@ -22,6 +27,7 @@ const schema = `
 		name TEXT NOT NULL,
 		description TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_category_locale_id_name ON category(locale_id, name);
 
 	CREATE TABLE IF NOT EXISTS information (
 		id INTEGER PRIMARY KEY NOT NULL,
@@ -29,30 +35,35 @@ const schema = `
 		title TEXT NOT NULL,
 		content TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_information_category_id_title ON information(category_id, title);
 
 	CREATE TABLE IF NOT EXISTS checklist (
 		id INTEGER PRIMARY KEY NOT NULL,
 		category_id INTEGER NOT NULL REFERENCES category(id),
 		title TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_checklist_category_id_title ON checklist(category_id, title);
 
 	CREATE TABLE IF NOT EXISTS checklist_item (
 		id INTEGER PRIMARY KEY NOT NULL,
 		checklist_id INTEGER NOT NULL REFERENCES checklist(id),
 		text TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_checklist_item_checklist_id_text ON checklist_item(checklist_id, text);
 
 	CREATE TABLE IF NOT EXISTS quiz (
 		id INTEGER PRIMARY KEY NOT NULL,
 		category_id INTEGER NOT NULL REFERENCES category(id),
 		title TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_quiz_category_id_title ON quiz(category_id, title);
 
 	CREATE TABLE IF NOT EXISTS quiz_question (
 		id INTEGER PRIMARY KEY NOT NULL,
 		quiz_id INTEGER NOT NULL REFERENCES quiz(id),
 		question TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_quiz_question_quiz_id_question ON quiz_question(quiz_id, question);
 
 	CREATE TABLE IF NOT EXISTS quiz_answer(
 		id INTEGER PRIMARY KEY NOT NULL,
@@ -60,6 +71,7 @@ const schema = `
 		answer TEXT NOT NULL,
 		is_correct INTEGER NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_quiz_answer_quiz_question_id_answer ON quiz_answer(quiz_question_id, answer);
 
 	CREATE TABLE IF NOT EXISTS image(
 		id INTEGER PRIMARY KEY NOT NULL,
@@ -67,9 +79,79 @@ const schema = `
 		related_id TEXT NOT NULL,
 		url TEXT NOT NULL
 	) STRICT;
+	CREATE UNIQUE INDEX IF NOT EXISTS idx_image_related_table_related_id_url ON image(related_table, related_id, url);
 
+	CREATE VIEW IF NOT EXISTS view_locale_language AS
+		SELECT locale.id as code_id, locale.code AS code, language.name AS language_name
+		FROM language
+		JOIN locale ON language.locale_id = locale.id
+		WHERE language.name = (
+			SELECT name 
+			FROM language l2 
+			WHERE l2.locale_id = locale.id
+			ORDER BY name
+			LIMIT 1
+		);
+
+	CREATE VIEW IF NOT EXISTS view_category AS
+		SELECT 
+			v.code,
+			v.language_name,
+			c.id AS category_id,
+			c.name AS category_name,
+			c.description AS category_description
+		FROM view_locale_language AS v
+		JOIN category AS c ON c.locale_id = v.code_id;
+	
+	CREATE VIEW IF NOT EXISTS view_information AS
+		SELECT 
+			v.code,
+			v.category_name,
+			i.title AS information_title,
+			i.content AS information_content
+		FROM view_category AS v
+		JOIN information AS i ON i.category_id = v.category_id;
+
+	CREATE VIEW IF NOT EXISTS view_checklist AS
+		SELECT 
+			v.code,
+			v.category_name,
+			c.title AS checklist_title,
+			GROUP_CONCAT(ci.text, ' ||| ') AS checklist_items
+		FROM 
+  		view_category AS v
+		JOIN checklist AS c ON c.category_id = v.category_id
+		JOIN checklist_item AS ci ON ci.checklist_id = c.id
+		GROUP BY 
+			v.code,
+			v.category_id,
+			v.category_name,
+			c.title;
+
+	CREATE VIEW IF NOT EXISTS view_quiz_question_answers AS
+		SELECT 
+			qq.id AS question_id,
+			qq.quiz_id,
+			qq.question,
+			GROUP_CONCAT(qa.answer || '::' || qa.is_correct, ' ||| ') AS answers
+	FROM quiz_question AS qq
+	JOIN quiz_answer AS qa ON qa.quiz_question_id = qq.id
+	GROUP BY question_id;                        
+	
+	CREATE VIEW IF NOT EXISTS view_full_quiz AS
+		SELECT 
+			vc.code,
+			vc.category_name,
+			vqa.quiz_id,
+			q.title AS quiz_title,
+			vqa.question,
+			vqa.answers
+		FROM view_quiz_question_answers AS vqa
+		JOIN quiz AS q ON vqa.quiz_id = q.id
+		JOIN view_category AS vc ON q.category_id = vc.category_id;
 `
-const db = new Database("swissway.db");
+
 db.run(schema);
+insertAll(db);
 
 export default db
